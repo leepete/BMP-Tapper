3abe48129465d6cd8b87896824d66e9d93f8f879
leepete <peterly@hotmail.co.nz>
2018-09-14 04:30:59 +1200

    Logic complete, working as expected. Could be more accurate though

--- Metronome/ViewController.swift
+++ Metronome/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapButton: UIButton!
     @IBOutlet weak var beatsPerMinuteLabel: UILabel!
     
-    var beatsPerMinute = 0 //result of the taps
+    var beatsPerMinute: Double = 0 //result of the taps
     
+    var timer = Timer() //Timer
+    var counter: Double = 0 //keep track of seconds
+    
+    var taps = 0 //keep track of taps
+    let maxTaps = 4 //max of 4 taps to determine and show BPM
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        beatsPerMinuteLabel.isHidden = true//Hide label name
         changeShapeButton(tapButton)
+        timerStart(tapButton)
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,21 +46,36 @@ class ViewController: UIViewController {
         sender.layer.cornerRadius = 0.5 * sender.bounds.size.width //getting circle needs half size of width
         sender.center.x = self.view.center.x //centre button by x axis
     }
+    
+    func timerStart(_ button: UIButton){
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(counterTrack), userInfo: nil, repeats: true)
+    }
+    
+    //Keep track of seconds - every interval, timerStart calls this method
+    @objc func counterTrack(){
+        counter += 0.001 //probably a better way to get milliseconds
+    }
 
-    @IBAction func countBeatsPerMinute(_ sender: UIButton) {
-        print(sender.tag) //DEBUG: Just checking if it registers
+    @IBAction func countBeatsPerMinute(_ button: UIButton) {
+        let seconds = counter
         
-        let max = 4 //max of 4 taps to determine BPM
-        let secInMins = 60
-        var count = 0 //keep track of taps
-        
-        while(count < max){
-            beatsPerMinute += 2
-            
-            count += 1
+        //Only allow a maximum of 4 taps before label gets changed
+        if(taps < maxTaps){
+            taps += 1
+            print(taps) //DEBUG
         }
         
-        beatsPerMinuteLabel.text = "\(beatsPerMinute)"
+        //If 4 taps have been registered
+        if(taps == maxTaps){
+            beatsPerMinuteLabel.isHidden = false //show label
+            
+            print(Double(60/seconds)) //DEBUG
+            beatsPerMinute = Double(60/seconds) * Double(maxTaps)
+            
+            beatsPerMinuteLabel.text = "\(Int(beatsPerMinute))" //change the text
+            taps = 0 //reset the number of taps
+            counter = 0 //reset timer
+        }
         
     }
     
